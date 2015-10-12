0acff7e735b5bc2f5aad88799c8bf764abdf54af
ekiscrim <xcstudiosonly@gmail.com>
2015-10-13 00:10:13 +0200

    diseño 1

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -4,21 +4,41 @@
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header colorMenu">
-				<a class="navbar-brand titulo" href="#">Dr. Manuel Sanchez Nebreda <div class="titulo2 h6">Cirugía Plástica y Estética</div></a>
-			</div>
-
-			<div>
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">Inicio</a></li>
-					<li><a href="#">Servicios</a></li>
-					<li><a href="#">Equipo</a></li>
-					<li><a href="#">Galería</a></li>
-					<li><a href="#">Quiénes somos</a></li>
-					<li><a href="#">Contacto</a></li>
+	<header>
+		<div class="container">
+			<h1 class="navbar-brand navbar-brand_ logo">
+				<a href="#">
+					<img alt="" src="assets/img/logo.png"></img>
+				</a>
+			</h1>
+			<nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+				<ul class="nav sf-menu clearfix">
+					<li class="active sub-menu">
+						<a href="#">home
+							<em></em>
+							<span></span>
+						</a>
+							<li>
+								<a href="#">historia</a>
+							</li>
+							<li>
+								<a href="#">staff</a>
+							</li>
+					</li>
+					
+					<li id="null">
+						<a href="#">servicios</a>
+					</li>
+					<li id="null">
+						<a href="#">staff</a>
+					</li>
+					<li id="null">
+						<a href="#">links</a>
+					</li>
+					<li id="null">
+						<a href="#">contacto</a>
+					</li>
 				</ul>
-			</div>
-		</div>
-	</nav>
+			</nav>
+	</header>
+	
