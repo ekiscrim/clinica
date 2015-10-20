c0eaad09c5e25f74f44662edce404dc38c7d951e
ekiscrim <xcstudiosonly@gmail.com>
2015-10-21 00:57:29 +0200

    Añadidas las 3 columnas
    Añadidas las 3 columnas de información, imágenes de prueba y sus
    respectivos botones.

--- application/views/home.php
+++ application/views/home.php
@@ -14,7 +14,49 @@
     <div class="sombra"></div>
     <div class="container contenido">
       <div class="row">
-        <div class="col-md-4"><h3>Cirugía estética de las mamas</h3></div>
-        <div class="col-md-4"><h3>Cirugía estética de la nariz</h3></div>
-        <div class="col-md-4"><h3>Liposucción</h3></div>
+        <div class="col-md-4 ciru">
+            <h3>Cirugía estética de las mamas</h3>
+            <div class="col-md-4 imagen1 img-responsive">
+                <img src="http://www.seedsofpeace.org/wp-content/uploads/2012/03/test.gif"></img>
+            </div>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec
+                lorem ullamcorper, scelerisque turpis id, vestibulum neque. Mauris in sapien ac
+                 nunc fringilla rutrum a at eros. Quisque ornare augue sit amet nulla efficitur rutrum.
+                 Duis vestibulum ligula ex, ac finibus purus iaculis a. Integer vehicula feugiat lacus,
+                  nec dignissim ipsum dictum ut. Nam vitae ipsum condimentum, efficitur erat at,
+                   finibus nisi.
+            </p>
+             <button type="button" class="btn btn-success leerMas">Leer más</button>
+       </div>
+        <div class="col-md-4 ciru">
+            <h3>Cirugía estética de la nariz</h3>
+            <div class="col-md-4 imagen1 img-responsive">
+                <img src="http://www.seedsofpeace.org/wp-content/uploads/2012/03/test.gif"></img>
+
+            </div>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec
+                lorem ullamcorper, scelerisque turpis id, vestibulum neque. Mauris in sapien ac
+                 nunc fringilla rutrum a at eros. Quisque ornare augue sit amet nulla efficitur rutrum.
+                 Duis vestibulum ligula ex, ac finibus purus iaculis a. Integer vehicula feugiat lacus,
+                  nec dignissim ipsum dictum ut. Nam vitae ipsum condimentum, efficitur erat at,
+                   finibus nisi.
+            </p>
+             <button type="button" class="btn btn-success leerMas">Leer más</button>
+       </div>
+        <div class="col-md-4 ciru">
+            <h3>Liposucción</h3>
+            <div class="col-md-4 imagen1 img-responsive">
+                <img src="http://www.seedsofpeace.org/wp-content/uploads/2012/03/test.gif"></img>
+            </div>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec
+                lorem ullamcorper, scelerisque turpis id, vestibulum neque. Mauris in sapien ac
+                 nunc fringilla rutrum a at eros. Quisque ornare augue sit amet nulla efficitur rutrum.
+                 Duis vestibulum ligula ex, ac finibus purus iaculis a. Integer vehicula feugiat lacus,
+                  nec dignissim ipsum dictum ut. Nam vitae ipsum condimentum, efficitur erat at,
+                   finibus nisi.
+            </p>
+             <button type="button" class="btn btn-success leerMas">Leer más</button>
+        </div>
+        </div>
       </div>
+      <hr>
